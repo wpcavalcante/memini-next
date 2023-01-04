47b52211fc728336fe25526cd7eb966a03f1ef63
unknown <wipcavalcante@gmail.com>
2023-01-03 21:05:24 -0300

    NextAuth autentication

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -11,7 +11,7 @@ export default NextAuth({
         }),
     ],
 
-        secret: process.env.NEXTAUTH_SECRET,
+        secret: process.env.NEXT_PUBLIC_SECRET,
 
        callbacks:{
         async session({session, profile}: any){
